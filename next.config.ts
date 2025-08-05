6cbf37d509b241c23a70355377573d118a58f088
uyiosa2607 <aghahowaosamudiamen03@gmail.com>
2025-08-05 13:18:29 +0100

    changes made

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  images: {
-    domains: ["cdn.durable.co"],
-  },
+  unoptimized: true,
 };
 
 export default nextConfig;
